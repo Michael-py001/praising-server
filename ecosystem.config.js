6578e72d22bd0cf3451ebc5babdee207c78f2104
codexu <461229187@qq.com>
2024-01-22 14:21:31 +0800

    feat: pm2 每日重启配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'praising-server',
       port: '3000',
       exec_mode: 'cluster',
+      cron_restart: '50 23 * * *',
       instances: 'max',
       script: './dist/main.js',
     },
